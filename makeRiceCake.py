3243ee26f62142939d9d08e1325836d2636d2474
ByungYeon Jang <kisagge@naver.com>
2020-08-26 14:45:38 +0900

    떡볶이 떡 만들기

---
+++ makeRiceCake.py
@@ -0,0 +1,27 @@
+TC = int(input())   
+ans = []
+for tc in range(1, TC + 1):
+    k = "#"+str(tc) + " "
+    
+    n, m = list(map(int, input().split()))
+    array = list(map(int, input().split()))
+
+    start = 0
+    end = max(array)
+
+    result = 0
+    while start <= end:
+        total = 0
+        mid = (start + end) // 2
+        for x in array:
+            if x > mid:
+                total += x - mid
+        if total < m:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+    k += str(result)
+    ans.append(k)
+for e in ans:
+    print(e)
